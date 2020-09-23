bd7fca98ac49aa93b5bbe9747da89a8c677aaa28
Polushkin Vladimir <polushkinvova@mail.ru>
2020-09-23 22:10:28 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            foreach (int i in numbers)
+            {
+                if (i==6)
+                {
+                    label1.Text = (i.ToString());
+                }
+               
+                
+            }
+        }
+    }
+}
